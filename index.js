f8198bf2f620c179e674dbfed69c5ac9434bf5f2
psneo7 <prateek.jsr.97@gmail.com>
2021-08-20 00:36:14 +0530

    added li template and toaster to stop addition of empty todos

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ todo_input = document.getElementById("todoInput")
 displayArea = document.getElementById("displayArea")
 todoListArea = document.getElementById("todoListArea")
 sortBtn = document.getElementById("sortBtn")
+toaster = document.getElementById("todoToaster")
 // class todo{
 //     desc: ,
 
@@ -25,6 +26,20 @@ function setLocalTodos() {
 }
 
 function createTodo(todo) {
+    // console.log("create todo called");
+    if (!todo) {
+        console.log("todo not available");
+        toaster.style = "display:block;opacity:1"
+        // toaster.style = "visibility:block"
+        setTimeout(() => {
+            toaster.style = "opacity:0"
+        }, 500);
+        setTimeout(() => {
+            toaster.style = "display:none"
+        }, 550);
+        return
+    }
+
     local_todos.push({
         id: local_todos.length,
         desc: todo,
@@ -62,8 +77,9 @@ let render = () => {
     local_todos.forEach(element => {
         console.log(element, "elements to be rendered");
         newListItem = document.createElement("li");
-        text = document.createTextNode(element.desc);
-        newListItem.appendChild(text);
+        newListItem.innerHTML = `${element.desc} <button id="toggleTodoButton" data-type="toggle-btn">${element.status ? "X" : "✓"}</button> <button data-type="del-btn" id="deleteTodoButton">Delete</button>`
+        // text = document.createTextNode(element.desc);
+        // newListItem.appendChild(text);
         newListItem.dataset.type = "todoEle";
         newListItem.dataset.todoId = element.id;
         if (element.status) {
@@ -80,11 +96,27 @@ let render = () => {
 }
 
 todoListArea.addEventListener('click', (e) => {
-    if (e.target.dataset.type == 'todoEle') {
-        console.log(e, "event recieved on click of li")
-        toggleTodoStatus(e.target.dataset.todoId)
+    if (e.target.dataset.type == 'toggle-btn') {
+        // console.log(e.target.parentNode.dataset, "event recieved on click of li")
+        toggleTodoStatus(e.target.parentNode.dataset.todoId)
+    }
+
+    if (e.target.dataset.type == 'del-btn') {
+        // console.log(e.target.parentNode.dataset, "event recieved on click of li")
+        deleteLocalTodo(e.target.parentNode.dataset.todoId)
     }
 })
+
+function deleteLocalTodo(todoId) {
+    console.log("id recived = ", todoId);
+    local_todos.forEach((element, index) => {
+        if (element.id == todoId) {
+            local_todos.splice(index, 1)
+        }
+    });
+    setLocalTodos()
+    render()
+}
 
 function toggleTodoStatus(todoId) {
     console.log("id recived = ", todoId);
